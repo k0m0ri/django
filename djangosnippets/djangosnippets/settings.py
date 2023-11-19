cde6d109a8256dbdb77a5d8a2a6cacbc52df34ee
k0m0ri <komori.sc14@gmail.com>
2023-11-19 23:23:46 +0900

    Update settings.py
    コメントを修正

--- djangosnippets/djangosnippets/settings.py
+++ djangosnippets/djangosnippets/settings.py
@@ -27,7 +27,6 @@ environ.Env.read_env(os.path.join(f"{BASE_DIR}/djangosnippets/", ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 # False if not in os.environ because of casting above
 DEBUG = env("DEBUG")
 
