92282e04550e70e7c34f0950c9571e6038a693e2
k0m0ri <komori.sc14@gmail.com>
2023-11-23 19:49:40 +0900

    作成と編集機能を追加

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -64,6 +64,18 @@ class SnippetDetailTest(TestCase):
             description="解説",
             created_by=self.user,
         )
+        self.client.force_login(self.user)  # ユーザーログイン
+
+    def test_render_creation_form(self):
+        response = self.client.get("/snippets/new/")
+        self.assertContains(response, "スニペットの登録", status_code=200)
+
+    def test_create_snippet(self):
+        data = {"title": "タイトル", "code": "コード", "description": "解説"}
+        self.client.post("/snippets/new/", data)
+        snippet = Snippet.objects.get(title="タイトル")
+        self.assertEqual("コード", snippet.code)
+        self.assertEqual("解説", snippet.description)
 
     def test_should_use_expected_template(self):
         response = self.client.get("/snippets/%s/" % self.snippet.id)
